578e940fa5ca7da6292dc8ade86789e731904e65
Joar Hussain <joarhussain@gmail.com>
2024-11-09 13:27:09 -0800

    Removed Dualshock4 files, will rely on build-in joy node

---
+++ build/p9n/colcon_command_prefix_build.sh
@@ -0,0 +1,6 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/joar/subvision_ws/install/p9n_interface/share/p9n_interface/package.sh"
+. "/home/joar/subvision_ws/install/p9n_example/share/p9n_example/package.sh"
+. "/home/joar/subvision_ws/install/p9n_node/share/p9n_node/package.sh"
+. "/home/joar/subvision_ws/install/p9n_test/share/p9n_test/package.sh"
+. "/home/joar/subvision_ws/install/p9n_bringup/share/p9n_bringup/package.sh"
---
+++ build/p9n_node/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/joar/subvision_ws/install/p9n_interface/share/p9n_interface/package.sh"
